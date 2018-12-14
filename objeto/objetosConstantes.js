4a7287ceb2c40c60bb6e12e232e7b6c70682a8bc
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2018-12-14 16:28:31 -0200

    Objetos Constantes

---
+++ objeto/objetosConstantes.js
@@ -0,0 +1,17 @@
+// Pessoa -> 123 -> {...}
+const pessoa = { nome: 'Joao'}
+pessoa.nome = 'Pedro'
+console.log(pessoa)
+
+// Pessoa <- 456 -> {...}
+// pessoa = {nome: 'Ana'}
+
+Object.freeze(pessoa)
+
+pessoa.nome = 'Maria'
+pessoa.end = 'Rua ABC'
+console.log(pessoa.nome)
+
+const pessoaConstante = Object.freeze({nome: 'Joao'})
+pessoaConstante.nome = 'Maria'
+console.log(pessoaConstante)
