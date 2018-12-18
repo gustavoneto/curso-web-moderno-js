66d07384e3c9118f368ba5f8ca9037caa5790a49
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2018-12-18 09:33:58 -0200

    testando erros

--- objeto/funcoesImportantes.js
+++ objeto/funcoesImportantes.js
@@ -25,9 +25,7 @@ console.log(Object.keys(pessoa))
 // Object.assign (ECMAScript 2015)
 const dest = {a: 1}
 const o1 = {b: 2}
-const o2 = {c:3, a:4}
-const obj = Object.assign(dest, o1, o2)
+const oeoso = {c:3, a:4}
+const obj = Object.assign(dest, o1, o3)
 
-Object.freeze(obj)
-obj.c = 1234
-console.log(obj)
+teste
