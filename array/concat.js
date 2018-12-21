79c4cd24c25104a213f836e35d019656ca90b885
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2018-12-21 15:05:25 -0200

    Exercícios feitos até o dia 21/12, encerrando a parte ARRAY

---
+++ array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Ualeskah', 'Cibelena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhos, filhas)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
