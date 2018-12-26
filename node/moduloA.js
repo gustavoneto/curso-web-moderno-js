33a0f4ee98999c8159c5c7ad0248960afb4f2b40
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2018-12-26 17:44:31 -0200

    Exercícios feito dia 26/12/2018

---
+++ node/moduloA.js
@@ -0,0 +1,6 @@
+// Em Node, um arquivo representa um módulo.
+// this é um objeto vazio {}
+
+this.ola = 'Fala Pessoal'
+exports.bemVindo = 'Bem vindo ao node!'
+module.exports.ateLogo = 'Até o próximo exemplo'
---
+++ node/usandoModulosTerceiros.js
@@ -0,0 +1,3 @@
+const _ = require('lodash')
+784
+setInterval(() => console.log(_.random(50, 60)), 2000)
