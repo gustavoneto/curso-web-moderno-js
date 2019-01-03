45384c21b59e248bfdfb798ecc23ee3d499cd2e5
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2019-01-03 10:05:24 -0200

    Revisão dos exercícios 'Organização' e 'Aritméticos' de 'Fundamentos'

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,23 @@
+/**
+ * Uma linha de código pode ser composta por várias linhas;
+ * Pode ou não terminar com ponto e virgula;
+ * Também é organizado em blocos de código, ou seja, agrupador;
+ * Pode ter um bloco dentro do outro {{}};
+ * No JS, a identação não influencia no código.
+ */
+
+console.log("Sentença de código")
+console.log()
+console.log("Olá"); // Duas sentenças para uma mesma palavra, ela será dividida
+console.log('Mundo!') // Padrão do curso
+
+/**  Não pode! Quebrar linha no meio de um print com aspas:
+ * 
+ * console.log("Sentença de 
+ * código")
+ * 
+ * O jeito certo seria:
+ * 
+ * console.log(
+ * "Sentença de código")
+ */
