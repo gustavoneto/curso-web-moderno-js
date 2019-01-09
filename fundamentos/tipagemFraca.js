f92366582d8538d3b47b5f09b863119efccd1558
Gustavo Neto <gustavo.neto@eticasolucoes.com.br>
2019-01-09 14:29:02 -0200

    Revisão de 'tipagemFraca' e 'variaveisEConstantes' feita no dia 09/01/2019

--- fundamentos/tipagemFraca.js
+++ fundamentos/tipagemFraca.js
@@ -1,10 +1,13 @@
 let qualquer = 'Legal'
-console.log(qualquer)
-console.log(typeof qualquer)
+console.log(`A valor da variável 'qualquer' é = ${qualquer}`)
+// mostrar o tipo da variável 'qualquer'
+console.log(`O tipo da variável 'qualquer' é = ${typeof qualquer}`)
+console.log('\n======= Mudando o tipo da variável =======\n')
 
-qualquer = 3.1516
-console.log(qualquer)
-console.log(typeof qualquer)
+qualquer = 3.1516 // A variável 'qualquer' mudou a partir daqui
+console.log(`A valor da variável 'qualquer' é = ${qualquer}`)
+// O tipo do novo valor da variável 'qualquer' mudou também
+console.log(`Agora o tipo da variável 'qualquer' é = ${typeof qualquer}`)
 
 //Evitar nomes genéricos e siglas
 let valor = ''
--- fundamentos/variaveisEConstantes.js
+++ fundamentos/variaveisEConstantes.js
@@ -2,8 +2,8 @@ var a = 3
 let b = 4
 
 var a = 30
-//let b = 40    não é possível redefinir uma variável utilizando let
-b = 40
+//let b = 40 não é possível redefinir uma variável utilizando let
+b = 40 // Utilizando 'var' pode-se mudar o valor da variável
 
 console.log(a, b)
 
@@ -13,5 +13,5 @@ b = 400
 console.log(a, b)
 
 const c = 5
-// c = 30       constante não se muda o valor
+// c = 30 constante não se muda o valor
 console.log(c) 
