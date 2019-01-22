f0bc14a05de11721928e798ef6f57745fb61b161
Gustavo Neto <gustavopcn@gmail.com>
2019-01-22 17:48:35 -0200

    Exercício revisado e modificado em 22/01/2019

--- fundamentos/numerosAlgunsCuidados.js
+++ fundamentos/numerosAlgunsCuidados.js
@@ -1,8 +1,8 @@
 console.log(7 / 0)  //JS tem um tipo Infinity
-console.log("10" / 2)
+console.log("10" / 2) /// JS reconhece um string com números
 console.log('3' + 2)    //não irá somar, irá contatenar
 console.log('3' - 2)
-console.log("Show!" * 2)
+console.log("Show!" * 2) // JS não reconhece o conteúdo da string
 console.log(0.1 + 0.7)
-//console.log(10.toString())
-console.log((10.345).toFixed(2))
+//console.log(10.toString()) // não dá pra chamar o literal.função
+console.log((10.345).toFixed(2)) // dá pra chamar o literal dentro de parentese.função
